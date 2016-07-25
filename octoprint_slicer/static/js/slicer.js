bdbbc1b2122386337c15fd87e93ab76e6b1e89c3
Kenneth Jiang <kenneth.jiang@gmail.com>
2016-07-25 01:17:33 +0000

    cookiecutter boilerplate

---
+++ octoprint_slicer/static/js/slicer.js
@@ -0,0 +1,28 @@
+/*
+ * View model for OctoPrint-Slicer
+ *
+ * Author: Kenneth Jiang
+ * License: AGPLv3
+ */
+$(function() {
+    function SlicerViewModel(parameters) {
+        var self = this;
+
+        // assign the injected parameters, e.g.:
+        // self.loginStateViewModel = parameters[0];
+        // self.settingsViewModel = parameters[1];
+
+        // TODO: Implement your plugin's view model here.
+    }
+
+    // view model class, parameters for constructor, container to bind to
+    OCTOPRINT_VIEWMODELS.push([
+        SlicerViewModel,
+
+        // e.g. loginStateViewModel, settingsViewModel, ...
+        [ /* "loginStateViewModel", "settingsViewModel" */ ],
+
+        // e.g. #settings_plugin_slicer, #tab_plugin_slicer, ...
+        [ /* ... */ ]
+    ]);
+});
