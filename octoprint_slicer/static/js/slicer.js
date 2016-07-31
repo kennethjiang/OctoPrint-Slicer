38f61df11dd13fc5e0b0e6be8a274bd4f93ad789
Kenneth Jiang <kenneth.jiang@gmail.com>
2016-07-31 23:44:56 +0000

    buttons on top of canvas

--- octoprint_slicer/static/js/slicer.js
+++ octoprint_slicer/static/js/slicer.js
@@ -52,14 +52,11 @@ $(function() {
             // ASCII file
 
             var loader = new THREE.STLLoader();
-            loader.load( 'http://threejs.org/examples/models/stl/ascii/slotted_disk.stl', function ( geometry ) {
+            loader.load(BASEURL + "downloads/files/" + "local" + "/" + "fish_fossilz.stl", function ( geometry ) {
 
                 var material = new THREE.MeshPhongMaterial( { color: 0xff5533, specular: 0x111111, shininess: 200 } );
                 var mesh = new THREE.Mesh( geometry, material );
-
-                mesh.position.set( 0, - 0.25, 0.6 );
-                mesh.rotation.set( 0, - Math.PI / 2, 0 );
-                mesh.scale.set( 0.5, 0.5, 0.5 );
+                mesh.scale.set( 0.02, 0.02, 0.02 );
 
                 scene.add( mesh );
             } );
@@ -78,7 +75,47 @@ $(function() {
             renderer.gammaInput = true;
             renderer.gammaOutput = true;
 
-            container.appendChild( renderer.domElement );
+    		$("#slicer-viewport").empty().append(`
+    			<div class="model">
+    				<input type="file" accept=".stl, .obj, .m3d, .amf, .wrl, .dae, .3mf">
+    				<button class="import" title="Import"><img src="` + PLUGIN_BASEURL + `m33fio/static/img/import.png"></button>
+    				<button class="translate disabled" title="Translate"><img src="` + PLUGIN_BASEURL + `m33fio/static/img/translate.png"></button>
+    				<button class="rotate" title="Rotate"><img src="` + PLUGIN_BASEURL + `m33fio/static/img/rotate.png"></button>
+    				<button class="scale" title="Scale"><img src="` + PLUGIN_BASEURL + `m33fio/static/img/scale.png"></button>
+    				<button class="snap" title="Snap"><img src="` + PLUGIN_BASEURL + `m33fio/static/img/snap.png"></button>
+    				<button class="delete disabled" title="Delete"><img src="` + PLUGIN_BASEURL + `m33fio/static/img/delete.png"></button>
+    				<button class="clone disabled" title="Clone"><img src="` + PLUGIN_BASEURL + `m33fio/static/img/clone.png"></button>
+    				<button class="reset disabled" title="Reset"><img src="` + PLUGIN_BASEURL + `m33fio/static/img/reset.png"></button>
+    				<button class="cut" title="Cut"><img src="` + PLUGIN_BASEURL + `m33fio/static/img/cut.png"></button>
+    				<button class="merge" title="Merge"><img src="` + PLUGIN_BASEURL + `m33fio/static/img/merge.png"></button>
+    			</div>
+    			<div class="display">
+    				<button class="boundaries" title="Boundaries"><img src="` + PLUGIN_BASEURL + `m33fio/static/img/boundaries.png"></button>
+    				<button class="measurements" title="Measurements"><img src="` + PLUGIN_BASEURL + `m33fio/static/img/measurements.png"></button>
+    			</div>
+    			<div class="values translate">
+    				<div>
+    					<p><span class="axis x">X</span><input type="number" step="any" name="x"><span></span></p>
+    					<p><span class="axis y">Y</span><input type="number" step="any" name="y"><span></span></p>
+    					<p><span class="axis z">Z</span><input type="number" step="any" name="z"><span></span></p>
+    					<span></span>
+    				</div>
+    			</div>
+    			<div class="cutShape">
+    				<div>
+    					<button class="cube disabled" title="Cube"><img src="` + PLUGIN_BASEURL + `m33fio/static/img/cube.png"></button>
+    					<button class="sphere" title="Sphere"><img src="` + PLUGIN_BASEURL + `m33fio/static/img/sphere.png"></button>
+    					<span></span>
+    				</div>
+    			</div>
+    			<div class="measurements">
+    				<p class="width"></p>
+    				<p class="depth"></p>
+    				<p class="height"></p>
+    			</div>
+    		`);
+    
+		    $("#slicer-viewport").append(renderer.domElement);
             controls = new THREE.OrbitControls(camera, renderer.domElement);
             controls.enableDamping = true;
             controls.dampingFactor = 0.25;
@@ -97,8 +134,6 @@ $(function() {
 
         }
 
-
-//        self.loadSTL(BASEURL + "downloads/files/" + "local" + "/" + "fish_fossilz.stl");
     }
 
     // view model class, parameters for constructor, container to bind to
