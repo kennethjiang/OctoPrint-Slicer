cf81536e6a6153748041d8beb2680ec23e2289b6
Kenneth Jiang <kenneth.jiang@gmail.com>
2016-08-01 22:19:03 +0000

    assets in slicer plugin path

--- octoprint_slicer/static/js/slicer.js
+++ octoprint_slicer/static/js/slicer.js
@@ -63,29 +63,15 @@ $(function() {
 
     		$("#slicer-viewport").empty().append(`
     			<div class="model">
-    				<button class="import" title="Import"><img src="` + PLUGIN_BASEURL + `m33fio/static/img/import.png"></button>
-    				<button class="translate disabled" title="Translate"><img src="` + PLUGIN_BASEURL + `m33fio/static/img/translate.png"></button>
-    				<button class="rotate" title="Rotate"><img src="` + PLUGIN_BASEURL + `m33fio/static/img/rotate.png"></button>
-    				<button class="scale" title="Scale"><img src="` + PLUGIN_BASEURL + `m33fio/static/img/scale.png"></button>
-    				<button class="snap" title="Snap"><img src="` + PLUGIN_BASEURL + `m33fio/static/img/snap.png"></button>
-    				<button class="delete disabled" title="Delete"><img src="` + PLUGIN_BASEURL + `m33fio/static/img/delete.png"></button>
-    				<button class="clone disabled" title="Clone"><img src="` + PLUGIN_BASEURL + `m33fio/static/img/clone.png"></button>
-    				<button class="reset disabled" title="Reset"><img src="` + PLUGIN_BASEURL + `m33fio/static/img/reset.png"></button>
-    				<button class="cut" title="Cut"><img src="` + PLUGIN_BASEURL + `m33fio/static/img/cut.png"></button>
-    				<button class="merge" title="Merge"><img src="` + PLUGIN_BASEURL + `m33fio/static/img/merge.png"></button>
+    				<button class="translate disabled" title="Translate"><img src="` + PLUGIN_BASEURL + `slicer/static/img/translate.png"></button>
+    				<button class="rotate" title="Rotate"><img src="` + PLUGIN_BASEURL + `slicer/static/img/rotate.png"></button>
+    				<button class="scale" title="Scale"><img src="` + PLUGIN_BASEURL + `slicer/static/img/scale.png"></button>
     			</div>
     			<div class="values translate">
     				<div>
     					<p><span class="axis x">X</span><input type="number" step="any" name="x"><span></span></p>
     					<p><span class="axis y">Y</span><input type="number" step="any" name="y"><span></span></p>
     					<p><span class="axis z">Z</span><input type="number" step="any" name="z"><span></span></p>
-    					<span></span>
-    				</div>
-    			</div>
-    			<div class="cutShape">
-    				<div>
-    					<button class="cube disabled" title="Cube"><img src="` + PLUGIN_BASEURL + `m33fio/static/img/cube.png"></button>
-    					<button class="sphere" title="Sphere"><img src="` + PLUGIN_BASEURL + `m33fio/static/img/sphere.png"></button>
     					<span></span>
     				</div>
     			</div>
