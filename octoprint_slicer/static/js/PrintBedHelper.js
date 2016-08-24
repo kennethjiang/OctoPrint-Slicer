0e7bba471753017fa004932b77ed618ece7a2ce4
Kenneth Jiang <kenneth.jiang@gmail.com>
2016-08-24 16:18:52 +0000

    checkered floor

--- octoprint_slicer/static/js/PrintBedHelper.js
+++ octoprint_slicer/static/js/PrintBedHelper.js
@@ -1,12 +1,12 @@
     bedFloor = function ( segments ) {
-        segments = segments || 8;
-        var geometry = new THREE.PlaneGeometry(1, 1, segments, segments);
+        segments = segments || 20;
+        var geometry = new THREE.PlaneGeometry(4, 4, segments, segments);
         var materialEven = new THREE.MeshBasicMaterial({color: 0xccccfc});
         var materialOdd = new THREE.MeshBasicMaterial({color: 0x444464});
         var materials = [materialEven, materialOdd];
     
         for (var x = 0; x < segments; x++) {
-          for (var y = 0; x < segments; x++) {
+          for (var y = 0; y < segments; y++) {
             var i = x * segments + y;
             var j = 2 * i;
             geometry.faces[ j ].materialIndex = geometry.faces[ j + 1 ].materialIndex = (x + y) % 2;
