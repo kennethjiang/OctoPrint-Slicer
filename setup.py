78e5cb38d28d7daad9ffe77eaa52afbbd02686e2
Kenneth Jiang <kenneth.jiang@gmail.com>
2016-09-26 06:51:39 -0700

    v0.4.0

--- setup.py
+++ setup.py
@@ -11,10 +11,10 @@ plugin_package = "octoprint_slicer"
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
-plugin_name = "Full-featured Slicer"
+plugin_name = "OctoPrint-Slicer"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "0.1.0"
+plugin_version = "0.4.0"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
