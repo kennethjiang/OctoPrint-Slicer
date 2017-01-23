5daaf6e0326e6b5a298eea53dd8acff7b86b23fc
Eyal Soha <eyalsoha@gmail.com>
2017-01-23 23:09:05 +0200

    Compute center of grouped STL correctly.

--- octoprint_slicer/static/js/slicer.js
+++ octoprint_slicer/static/js/slicer.js
@@ -477,7 +477,10 @@ $(function() {
             })) {
                 destinationFilename = destinationFilename + "." + destinationExtensions[0];
             }
-	    var groupCenter = new THREE.Box3().setFromObject(group).center();
+	    var groupCenter = new THREE.Vector3(0,0,0);
+	    if (group) {
+		groupCenter = new THREE.Box3().setFromObject(group).center();
+	    }
             var data = {
                 command: "slice",
                 slicer: slicingVM.slicer(),
@@ -505,7 +508,6 @@ $(function() {
 		    new PNotify({title: "Slicing failed", text: textStatus, type: "error", hide: false});
 		}
             });
-
 	}
 
         self.slice = function() {
@@ -522,8 +524,8 @@ $(function() {
 		    self.slicingViewModel.file().substring(extensionPosition);
 		var group = new THREE.Group();
 		_.forEach(self.stlFiles, function (stlFile) {
-		    var modelCopy = stlFile.model.clone(true); // recursive clone
-		    group.copy(stlFile.model, true);
+		    var modelCopy = stlFile.model.clone(true);
+		    group.add(modelCopy);
 		});
 		form.append("file", self.blobFromModel(group), newFilename);
 		$.ajax({
