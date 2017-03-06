c5bba257bbcd3b8e989a36535144baccfc1476e1
Kenneth Jiang <kenneth.jiang@gmail.com>
2017-03-06 08:09:30 -0800

    loop through all sorting criteria and pick the best

--- octoprint_slicer/static/js/ModelArranger.js
+++ octoprint_slicer/static/js/ModelArranger.js
@@ -14,33 +14,59 @@ var ModelArranger = {
         var modelBox = new THREE.Box3().setFromObject(modelClone);
         var width = modelBox.max.x - modelBox.min.x + margin;
         var height = modelBox.max.y - modelBox.min.y + margin;
-        return { w: width, h: height, model: model };
+        return { w: width, h: height, area: width*height, model: model };
     },
 
     arrange: function(models) {
-        var rects = models.map( ModelArranger.projectedRectOnXY );
 
-        // find a square that will likely fit all models
-        var totalArea = rects.reduce( function(area, rect) {
-            return area + rect.w * rect.h;
-        }, 0);
+        var criteria = ['w', 'h', 'a', 'max', 'min', 'height', 'width', 'area', 'maxside'];
+        var allPackers = criteria.map( function( crit ) {
 
-        var largestSide = rects.reduce( function(largest, rect) {
-            return Math.max(largest, rect.w, rect.h);
-        }, 0);
+            var rects = models.map( ModelArranger.projectedRectOnXY );
+            rects.sort( ModelArranger.sort[crit] );
 
-        var side = Math.max(largestSide, Math.sqrt(totalArea));
-
-        while ( rects.some( function(rect) { return !rect.fit; } ) ) {
-            side *= 1.1; // increase 10% if not fit
             var packer = new GrowingPacker();
-            rects.sort(function(a,b) { return (b.w * b.h > a.w * a.h); }); // sort inputs for best results
             packer.fit(rects);
-            rects.forEach( function( rect ) { ModelArranger.applyPackResult(rect, packer); });
+
+            return {packer: packer, rects: rects};
+        });
+
+        var packersSortedByArea = allPackers.filter( function( result ) {
+            return ! result.rects.some( function(rect) { return !rect.fit; } );
+        }).sort( function(a, b) {
+            return b.packer.root.area - a.packer.root.area;
+        });
+
+        if (packersSortedByArea.length > 0) {
+            packersSortedByArea[0].rects.forEach( function( rect ) { ModelArranger.applyPackResult(rect, packersSortedByArea[0].packer); });
+        }
+    },
+
+    sort: {
+
+        w       : function (a,b) { return b.w - a.w; },
+        h       : function (a,b) { return b.h - a.h; },
+        a       : function (a,b) { return b.area - a.area; },
+        max     : function (a,b) { return Math.max(b.w, b.h) - Math.max(a.w, a.h); },
+        min     : function (a,b) { return Math.min(b.w, b.h) - Math.min(a.w, a.h); },
+
+        height  : function (a,b) { return ModelArranger.sort.msort(a, b, ['h', 'w']);               },
+        width   : function (a,b) { return ModelArranger.sort.msort(a, b, ['w', 'h']);               },
+        area    : function (a,b) { return ModelArranger.sort.msort(a, b, ['a', 'h', 'w']);          },
+        maxside : function (a,b) { return ModelArranger.sort.msort(a, b, ['max', 'min', 'h', 'w']); },
+
+        msort: function(a, b, criteria) { /* sort by multiple criteria */
+            var diff, n;
+            for (n = 0 ; n < criteria.length ; n++) {
+                diff = ModelArranger.sort[criteria[n]](a,b);
+                if (diff != 0)
+                    return diff;
+            }
+            return 0;
         }
 
-
     }
+
 };
 
 // browserify support
