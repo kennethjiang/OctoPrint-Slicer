86bdc91efc6c9497ba3006dc196093ceab95e567
Kenneth Jiang <kenneth.jiang@gmail.com>
2017-04-06 14:36:15 -0700

    bump version

--- src/slicer.js
+++ src/slicer.js
@@ -22,7 +22,7 @@ function isDev() {
 
 if ( ! isDev() ) {
     Raven.config('https://85bd9314656d40da9249aec5a32a2b52@sentry.io/141297', {
-        release: '1.2.0',
+        release: '1.2.1',
         ignoreErrors: [
             "Failed to execute 'arc' on 'CanvasRenderingContext2D': The radius provided",
             "Cannot read property 'highlightFill' of undefined",
