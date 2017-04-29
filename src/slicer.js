3d3f2cda464b541d56f005cbdd80ee6a7f1a25e3
Eyal Soha <eyalsoha@gmail.com>
2017-04-29 19:03:40 +0300

    don't use the faster Box3 on the group because the model.clone doesn't work and it won't save much time anyway

--- src/slicer.js
+++ src/slicer.js
@@ -341,7 +341,7 @@ function SlicerViewModel(parameters) {
         }
         var groupCenter = new THREE.Vector3(0,0,0);
         if (group) {
-            groupCenter = group.userData.box3FromObject(group);
+            groupCenter = new THREE.Box3().setFromObject(group);
         }
         var data = {
             command: "slice",
@@ -393,7 +393,6 @@ function SlicerViewModel(parameters) {
             forEach(self.stlViewPort.models(), function (model) {
                 group.add(model.clone(true));
             });
-            group.userData.box3FromObject = Box3FromObject(group);
 
             sliceRequestData = self.sliceRequestData(self.slicingViewModel, group);
 
